879c3ca9795be87ff4a16561f900f2a146132627
Sweny_ <peter.benedik@protonmail.com>
2019-01-03 17:25:31 +0100

    Minor change
    Removing error from launcher.

--- assets/bin/launcher.sh
+++ assets/bin/launcher.sh
@@ -4,7 +4,6 @@ do
      sleep 5
 done
 echo "Trigger file found executing antivirus scan now!"
+rm -rf /data/av/queue/scan.now
 /usr/local/bin/scanner.sh
-echo "Last scan completed, starting new lauch daeomn!"
-/bin/bash /usr/local/bin/launcher.sh &
-exit 1
+exit
